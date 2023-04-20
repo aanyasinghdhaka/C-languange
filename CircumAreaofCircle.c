c49ee8a4e49c02c116f112e39f284ea3eb7b8b65
aanyasinghdhaka <122288528+aanyasinghdhaka@users.noreply.github.com>
2023-04-20 20:55:03 +0530

    Create CircumAreaofCircle.c

---
+++ CircumAreaofCircle.c
@@ -0,0 +1,11 @@
+int main()
+{
+    int  radius;
+    printf("Enter the radius\n");
+    scanf("%d",&radius);
+    float Area=((3.14)* radius*radius);
+    float Circumference=2*3.14*radius;
+    printf("Area and Circumference of circle=%f and %f", Area ,Circumference);
+
+    return 0;
+}
